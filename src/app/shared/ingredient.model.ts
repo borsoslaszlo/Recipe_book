3f8dd8f5e107eaf7e0c0e8863ef80d3f1d96a094
Laszlo-Borsos1 <laszlo.borsos@kyndryl.com>
2022-10-25 13:07:12 +0200

    Course project - The basics

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+
+    // automatically crates the fields of class and assignes the parameters to this
+    // class  .  Shorcut of explicit field an constructor definition 
+    constructor (public name: string, public amount: number)
+    {
+    }
+}
